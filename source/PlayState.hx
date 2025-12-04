8b9dd0876ce309069bd46695ac67f75b3711eafc
Viwu <sinconsistencia@gmail.com>
2025-12-04 16:14:04 -0600

    all works well and now we have a ball

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,12 +1,25 @@
 package;
 
+import flixel.util.FlxColor;
+import flixel.util.FlxSpriteUtil;
+import flixel.graphics.FlxGraphic;
+import flixel.FlxSprite;
 import flixel.FlxState;
 
 class PlayState extends FlxState
 {
+	public var player:FlxSprite;
+
 	override public function create()
 	{
 		super.create();
+
+		player = new FlxSprite().makeGraphic(16, 16, FlxColor.TRANSPARENT);
+		FlxSpriteUtil.drawCircle(player);
+		player.color = FlxColor.RED;
+		add(player);
+
+		player.screenCenter();
 	}
 
 	override public function update(elapsed:Float)
